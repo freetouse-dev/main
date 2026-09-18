0d56b980777565cc14e2ceb90b2d47439ae66bd7
freetouse-dev <gingergin348@gmail.com>
2026-09-18 11:01:04 +0530

    Delete welcome.php

--- welcome.php
+++
@@ -1,7 +0,0 @@
-<?php
-$name = $_GET["name"];
-$email = $_GET["email"];
-
-echo "<h2>Display Form data</h2>";
-echo "Welcome to DEVELOP, Mr/Mrs.<b>$name</b></br>";
-echo "Your integrated email was <b>$email</b></br>";
